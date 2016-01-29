769dfee57a4b02c5fe87c29931bdfa5f26216c44
anshu.wang <ricksnumber537@gmail.com>
2016-01-29 15:26:44 +0800

    修改MotionEvent的顺序

--- app/src/main/java/edu/ptu/androidtouchanalyse/data/MotionEventInfo.java
+++ app/src/main/java/edu/ptu/androidtouchanalyse/data/MotionEventInfo.java
@@ -7,6 +7,6 @@ import android.view.MotionEvent;
  * Created by WangAnshu on 2016/1/27.
  */
 public class MotionEventInfo {
-    public static String[] ACTION_NAME = {"ACTION_DOWN", "ACTION_UP", "ACTION_MOVE", "ACTION_CANCEL"};
-    public static int[] ACTION_TYPE = new int[]{MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL};
+    public static String[] ACTION_NAME = {"ACTION_DOWN",  "ACTION_MOVE", "ACTION_UP","ACTION_CANCEL"};
+    public static int[] ACTION_TYPE = new int[]{MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_CANCEL};
 }
